5e30df89acdc6d1bfc4358b44e04e8556edd7ece
Coraline Chu <xc492@cornell.edu>
2025-05-12 18:50:04 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,1 +1,14 @@
-function skillsMember()
+function skillsMember() {
+  return (
+    <div className="skills-member">
+      <h2>Skills</h2>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>CSS</li>
+        <li>HTML</li>
+      </ul>
+    </div>
+  );
+}
